88d21edc235abc0060a5ffff0481a5ea4cfc1a96
Shmuel <samuelwine@gmail.com>
2022-01-31 14:02:54 +0000

    Add Twilio nuget package and Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Conversations.V1;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var conversation = ConversationResource.Create(
+            friendlyName: "My First Conversation"
+        );
+
+        Console.WriteLine(conversation.Sid);
+    }
+}
